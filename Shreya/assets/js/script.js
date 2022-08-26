4ed50e6b7ffb4d0556c6e23fea53924a65c56a70
Shreya Saha <shreyasha7.3@gmail.com>
2022-08-26 18:38:15 +0530

    lazy

--- Shreya/assets/js/script.js
+++ Shreya/assets/js/script.js
@@ -1,81 +1,78 @@
 $(".main-menu li:first").addClass("active");
 
-        var showSection = function showSection(section, isAnimate) {
-            var direction = section.replace(/#/, ""),
-                reqSection = $(".section").filter(
-                    '[data-section="' + direction + '"]'
-                ),
-                reqSectionPos = reqSection.offset().top - 0;
+var showSection = function showSection(section, isAnimate) {
+    var direction = section.replace(/#/, ""),
+        reqSection = $(".section").filter(
+            '[data-section="' + direction + '"]'
+        ),
+        reqSectionPos = reqSection.offset().top - 0;
 
-            if (isAnimate) {
-                $("body, html").animate(
-                    {
-                        scrollTop: reqSectionPos
-                    },
-                    800
-                );
-            } else {
-                $("body, html").scrollTop(reqSectionPos);
-            }
-        };
+    if (isAnimate) {
+        $("body, html").animate(
+            {
+                scrollTop: reqSectionPos
+            },
+            800
+        );
+    } else {
+        $("body, html").scrollTop(reqSectionPos);
+    }
+};
 
-        var checkSection = function checkSection() {
-            $(".section").each(function () {
-                var $this = $(this),
-                    topEdge = $this.offset().top - 80,
-                    bottomEdge = topEdge + $this.height(),
-                    wScroll = $(window).scrollTop();
-                if (topEdge < wScroll && bottomEdge > wScroll) {
-                    var currentId = $this.data("section"),
-                        reqLink = $("a").filter("[href*=\\#" + currentId + "]");
-                    reqLink
-                        .closest("li")
-                        .addClass("active")
-                        .siblings()
-                        .removeClass("active");
+var checkSection = function checkSection() {
+    $(".section").each(function () {
+        var $this = $(this),
+            topEdge = $this.offset().top - 80,
+            bottomEdge = topEdge + $this.height(),
+            wScroll = $(window).scrollTop();
+        if (topEdge < wScroll && bottomEdge > wScroll) {
+            var currentId = $this.data("section"),
+                reqLink = $("a").filter("[href*=\\#" + currentId + "]");
+            reqLink
+                .closest("li")
+                .addClass("active")
+                .siblings()
+                .removeClass("active");
+        }
+    });
+};
+
+$(".main-menu").on("click", "a", function (e) {
+    e.preventDefault();
+    showSection($(this).attr("href"), true);
+});
+
+$(window).scroll(function () {
+    checkSection();
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var lazyloadImages = document.querySelectorAll("img.lazy");
+    var lazyloadThrottleTimeout;
+
+    function lazyload() {
+        if (lazyloadThrottleTimeout) {
+            clearTimeout(lazyloadThrottleTimeout);
+        }
+
+        lazyloadThrottleTimeout = setTimeout(function () {
+            var scrollTop = window.pageYOffset;
+            lazyloadImages.forEach(function (img) {
+                if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazy');
                 }
             });
-        };
+            if (lazyloadImages.length == 0) {
+                document.removeEventListener("scroll", lazyload);
+                window.removeEventListener("resize", lazyload);
+                window.removeEventListener("orientationChange", lazyload);
+            }
+        }, 20);
+    }
 
-        $(".main-menu").on("click", "a", function (e) {
-            e.preventDefault();
-            showSection($(this).attr("href"), true);
-        });
-
-        $(window).scroll(function () {
-            checkSection();
-        });
-
-
-const images = ["assets/images/city/3.jpg", "assets/images/city/4.jpg", "assets/images/city/5.jpeg"];
-// for (let x = 0; x < images.length; x++) {            
-//     const element = '<img src="' + images[x] + '" alt="sq-sample26"';
-//     document.querySelector('.images').innerHTML += element;
-//     const el = '<a href="'+ images[x] +'" data-lightbox="image-1"'+
-//     'data-title="Caption"><i class="fa fa-search"></i></a>'
-//     document.querySelector('.images1').innerHTML += element;
-// }
-// function page1() {
-//     return "<html><head><style></style></head><body class='heraldone'><\/script><\/body><\/html>";
-// }
-// const outElem = document.getElementById('gallery');
-// images.forEach(function (path) {
-//     const isotopeitem = document.createElement("div");
-//     isotopeitem.className = ".isotope-item";
-//     isotopeitem.setAttribute("data-type", "city");
-//     const fig = document.createElement("figure");
-//     fig.className = ".snip1321";
-//     const img = document.createElement("img");
-//     img.src = path;
-//     img.alt = "sample";
-//     const cap = document.createElement("figcaption");
-//     const anch = document.createElement("a");
-//     anch.href = path;
-//     anch.setAttribute("data-lightbox","image-1");
-//     anch.setAttribute("data-title","Caption");
-//     cap.appendChild(anch);
-//     fig.appendChild(img);
-//     fig.appendChild(cap);
-//     isotopeitem.appendChild(fig);
-//     outElem.appendChild(isotopeitem);
-//   })
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+});
