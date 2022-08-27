fefa519c92e9cf4b1582d35f2454309319e0b1cc
Shreya Saha <shreyasha7.3@gmail.com>
2022-08-27 18:36:09 +0530

    change

---
+++ Shreya/assets/js/works.js
@@ -0,0 +1,18 @@
+(function($) {
+$(".isotope-wrapper").each(function() {
+    var $isotope = $(".isotope-box", this);
+    var $filterCheckboxes = $('input[type="radio"]', this);
+
+    var filter = function() {
+      var type = $filterCheckboxes.filter(":checked").data("type") || "*";
+      if (type !== "*") {
+        type = '[data-type="' + type + '"]';
+      }
+      $isotope.isotope({ filter: type });
+    };
+
+    $(this).on("change", filter);
+    filter();
+  });
+
+})(jQuery);
